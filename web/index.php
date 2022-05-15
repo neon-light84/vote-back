9e836a726fd31a36debf302afc0161a85bf2eed1
neon-light84 <goroshnikov.pavel@gmail.com>
2022-05-15 21:46:18 +0300

    Добавил CORS

--- web/index.php
+++ web/index.php
@@ -1,4 +1,20 @@
 <?php
+//TODO: пока костыльный способ побороть CORS. Потом изыскать средства самого фреймворка
+$arrAllowedOrigin = [
+    'http://cors.front1.local.su',
+    'http://alex-team.ru',
+    'https://alex-team.ru',
+    'http://localhost',
+    'https://localhost',
+];
+if (in_array($_SERVER['HTTP_ORIGIN'], $arrAllowedOrigin)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+}
+header('Access-Control-Allow-Headers: ' . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+header('Access-Control-Allow-Methods: PATCH PUT DELETE OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 600');
+
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
